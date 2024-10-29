dbc28a76e99323323e71d0af49038e32f902abd9
wanhailiang <wanhailiangwhl@gmail.com>
2024-10-30 04:37:46 +0800

    删除了没用的文件

--- JavaWebProjectSimple/src/com/wanhailiang/msg/Message.java
+++
@@ -1,9 +0,0 @@
-package com.wanhailiang.msg;
-
-public class Message {
-    public static String USER_FOUND_BUT_PWD_FAILED = "密码错误！";
-    public static String USER_NOT_FOUND = "用户名不存在！";
-
-    public static String INSERT_SUCCESS = "插入成功！";
-    public static String INSERT_FAILED = "插入失败！";
-}
--- JavaWebProjectSimple/src/com/wanhailiang/msg/Status.java
+++
@@ -1,6 +0,0 @@
-package com.wanhailiang.msg;
-
-public class Status {
-    final public static int USER_NOT_FOUND = 10001;
-    final public static int USER_FOUND_BUT_PWD_FAILED = 10002;
-}
