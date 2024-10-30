455ce54dce408d3225aba7b4e25c58eb1e080d38
wanhailiang <wanhailiangwhl@gmail.com>
2024-10-31 01:19:16 +0800

    2024.10.31 提交单词通多模块初始化项目

---
+++ 单词通/dancitong/web/src/main/java/com/dancitong/web/WebApplication.java
@@ -0,0 +1,15 @@
+package com.dancitong.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(value = "com.dancitong")
+public class WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+
+}
